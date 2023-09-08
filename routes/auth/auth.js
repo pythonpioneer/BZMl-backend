6f4e11aefdfa0db5e6e63b14862a4fcf8ecffcd6
Hritik <pypi.pioneer@gmail.com>
2023-09-09 05:21:35 +0530

    user logged in api created

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,11 +1,18 @@
 // importing requirements
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 const { validateUserField } = require('../../controllers/validator');
-const { createUser } = require('../../controllers/user');
+const { createUser, loginUser } = require('../../controllers/user');
 
 
 // Route 1: To create user: '/api/v1/auth/user' [using POST] (login not required)
 router.post('/user', validateUserField, createUser);
+
+// Route 2: To login user: '/api/v1/auth/loginuser' [using POST] (login not required)
+router.post('/loginuser', [
+    body('userfield', 'enter valid mobileNumber/email/pubgID to login').isLength({min: 9, max: 50}),
+    body('password', "Enter valid password").isAlphanumeric().isLength({min: 6, max: 18})
+], loginUser);
 
 module.exports = router;
