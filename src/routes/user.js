0f799a6fbd70f6c6bb825800db90a65e43358ba9
Hritik <pypi.pioneer@gmail.com>
2023-09-15 19:54:15 +0530

    delete user API created

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,16 +1,19 @@
 // importing requirements
 const express = require('express');
 const { fetchUser } = require('../middleware/auth/authMiddleware');
-const { getUserDetails, setUserDetails } = require('../controllers/user');
-const { validateRegistrationField, validateUpdationField } = require('../middleware/validator/validateFormField');
+const { getUserDetails, setUserDetails, deleteUserAccount } = require('../controllers/user');
+const { validateUpdationField } = require('../middleware/validator/validateFormField');
 const { validateValidationResult } = require('../middleware/validator/validationMiddleware');
 const router = express.Router();
 
 // Route 3: To get logged in user detail: '/api/v1/user/getuser' [using GET] (login required)
 router.get('/getuser', fetchUser, getUserDetails);
 
-// Route 4: To update logged in user detail: '/api/v1/user/user-id=1thisisdemoid' [using PUT] (login required)
+// Route 4: To update logged in user detail: '/api/v1/user/user' [using PUT] (login required)
 router.put('/user', validateUpdationField, validateValidationResult, fetchUser, setUserDetails);
+
+// Route 4: To delete a logged in user account: '/api/v1/user/delete-user' [using DELETE] (login required)
+router.delete('/delete-user', fetchUser, deleteUserAccount);
 
 // export router
 module.exports = router;
