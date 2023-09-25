694f25a5257855ee79d7c00c6076b9a09a53888d
Hritik <pypi.pioneer@gmail.com>
2023-09-25 14:55:29 +0530

    setting up environment variables

--- src/connectionDB/db.js
+++ src/connectionDB/db.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// mongodb setup
 const mongoURI = 'mongodb://localhost:27017/bzml';
+console.log(process.env.MONGO_URI);
 
 const connectToMongo = async () => {
     mongoose.connect(mongoURI)
         .then(() => {
-            console.log("Coneection successfull, " + "connected to MongoDB");
+            console.info("Coneection successfull, " + "connected to MongoDB");
         })
         .catch((err) => {
             console.log("Coneection Interrupted");
--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ const connectToMongo = require('./connectionDB/db');
 const express = require('express');
 const cors = require("cors");
 
+// setting local variables
+const dotenv = require('dotenv');
+dotenv.config();
+
 // mongo connection
 connectToMongo();
 
+// development environment specifications
 const app = express()
-const PORT = 3200;
-
-const APIPATH = '/api/v1/';
+const PORT = process.env.PORT;
+const APIPATH = process.env.API_PATH;  // this can be any string, a starting path for the API
 
 // to use req.body, we have to use this middleware
 app.use(express.json());
