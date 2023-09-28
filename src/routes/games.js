5e7d92da9c91554a2733ce159c6235bd0e661f5b
Hritik <pypi.pioneer@gmail.com>
2023-09-29 01:21:52 +0530

    api created to get all current game list

--- src/routes/games.js
+++ src/routes/games.js
@@ -1,14 +1,14 @@
 // importing all requirements
 const express = require('express');
 const { body, validationResult } = require('express-validator');
-const { createGame } = require('../controllers/game');
+const { createGame, getGames } = require('../controllers/game');
 const { fetchUser } = require('../middleware/auth/authMiddleware');
 const { validateValidationResult } = require('../middleware/validator/validationMiddleware');
 
 // now creating router, to map all routes
 const router = express.Router();
 
-// Route 1: To create a game (only admin to access): '/bzml/api/v1/games/create-game' [using POST] (login not required)
+// Route 1: To create a game (only admin to access): '/bzml/api/v1/games/create-game' [using POST] (login required)
 router.post('/create-game', [
     body('gamingPlatform', 'Enter a valid platform (bgmi | pubg)').custom((value, {req}) => {
         return value ? ['bgmi', 'pubg'].includes(value?.toLowerCase()) : true  // if platform is missing then it's BGMI (default in db)
@@ -21,6 +21,9 @@ router.post('/create-game', [
     fetchUser,
     createGame
 );
+
+// Route 2: To get all current games list: '/bzml/api/v1/games/game' [using GET] (login not required)
+router.get('/game', fetchUser, getGames);
 
 // export the router
 module.exports = router;
