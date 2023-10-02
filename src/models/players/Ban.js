98b430a392a33f279d508f57d8a2fd40045ca052
Hritik <pypi.pioneer@gmail.com>
2023-10-02 22:03:46 +0530

    ban model created

---
+++ src/models/players/Ban.js
@@ -0,0 +1,26 @@
+// importing requirements
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// creating schema for users
+const banSchema = new Schema({
+    pubgID: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    mobileNumber: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+});
+
+// exporting the ban model
+const Ban = mongoose.model('ban', banSchema);
+module.exports = Ban;
