dbdb1d80eb8a4ab2e4cb171c1d538ded4033ca22
Hritik <pypi.pioneer@gmail.com>
2023-10-03 17:27:07 +0530

    now, we can ban the user by their pubgID

--- src/routes/ban.js
+++ src/routes/ban.js
@@ -6,14 +6,13 @@ const { banPlayer } = require('../controllers/ban');
 const { fetchUser } = require('../middleware/auth/authMiddleware');
 const { validateValidationResult } = require('../middleware/validator/validationMiddleware');
 
-// Route 1: To ban a user (admin access only): '/bzml/api/v1/ban/ban-player?user-id=<...>' [using POST] (login required)
+// Route 1: To ban a user (admin access only): '/bzml/api/v1/ban/ban-player' [using POST] (login required)
 router.post('/ban-player', [
-    body('password', "Enter a valid password (admin)")
-        .isAlphanumeric()
-        .isLength({ min: 6, max: 18 })
-], 
+    body('pubgID', 'Enter your PUBG/BGMI ID').isNumeric().isLength({ min: 9, max: 12 }),
+    body('password', "Enter a valid password (admin)").isAlphanumeric().isLength({ min: 6, max: 18 })
+],
     validateValidationResult,
-    fetchUser, 
+    fetchUser,
     banPlayer
 );
 
