79c1283f33c17329b91dd6cff9302f80e16eda18
Hritik <pypi.pioneer@gmail.com>
2023-10-15 00:39:37 +0530

    validation array optional fields added dynamically to email and password fields

--- src/helper/utility/validateFields/passwordField.js
+++ src/helper/utility/validateFields/passwordField.js
@@ -5,16 +5,21 @@ const { check } = require('express-validator');
 /**
  * This method is only used to validate the password fields.
  * @param {array} passwordNames - This method takes an array of password-names as input.
+ * @param {boolean} isOptional - provide optional as true, if want the validation array to become optional
  * @returns {array} - It returns validation array to validate password fields
  */
-const validatePassword = (passwordNames) => {  // the password names can contain many password name
+const validatePassword = (passwordNames, isOptional) => {  // the password names can contain many password name
 
     // check that the given input is array type
     if (!Array.isArray(passwordNames)) throw new Error('This method accepts input as an array only.');
     
     // traverse in the password Names
     return passwordNames.map(passwordName => {
-        return check(passwordName, `Enter a valid ${passwordName}`).isLength({ min: 6, max: 18 });
+        const validationChain = check(passwordName, `Enter a valid ${passwordName}`).isLength({ min: 6, max: 18 });
+
+        // making the validation array optional
+        if (isOptional) validationChain.optional();
+        return validationChain;
     });
 };
 
