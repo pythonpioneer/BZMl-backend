a00f7215de9716bc827ee324caef95c79d0a5b08
Hritik <pypi.pioneer@gmail.com>
2023-10-18 05:41:09 +0530

    sending otp to mobile number, implemented successfully

--- src/controllers/verify.js
+++ src/controllers/verify.js
@@ -2,9 +2,12 @@
 const User = require('../models/user/User');
 const Admin = require('../models/user/Admin');
 const EmailVerification = require('../models/verfiy/VerifyEmail');
+const MobileVerification = require('../models/verfiy/VerifyMobile');
 const RecoverPassword = require('../models/verfiy/RecoverPass');
 
 // importing other requirements
+const Twilio = require('twilio');
+const dotenv = require('dotenv').config();
 const { generateOtp } = require('../helper/utility/generate');
 const { sendMail } = require('../helper/utility/sendMail');
 const { generatePassword } = require('../middleware/auth/passwordMiddleware');
@@ -26,13 +29,13 @@ const verifyEmail = async (req, res) => {
         if (!user) return res.status(404).json({ status: 404, message: "User Not Found!" });
 
         // now, confirm that the user requested for OTP
-        let pin = await EmailVerification.findOne({ email }); 
+        let pin = await EmailVerification.findOne({ email });
         if (!pin) return res.status(400).json({ status: 400, message: "You need to Generate OTP" });
 
         // if OTP is generated then match the OTP
         if (otp !== pin.otpEmail) return res.status(400).json({ status: 400, message: "Invalid OTP" });
 
-        if (!user.isEmailVerified){  // now, verify the user
+        if (!user.isEmailVerified) {  // now, verify the user
             user.isEmailVerified = true;
             user.save();
 
@@ -44,7 +47,7 @@ const verifyEmail = async (req, res) => {
         }
 
     } catch (err) {  // any unrecogonize error will be raised from here
-        return res.status(500).json({ 
+        return res.status(500).json({
             status: 500,
             message: "Internal server error",
             issue: err
@@ -90,13 +93,13 @@ const generateEmailOtp = async (req, res) => {
                 });
 
                 // verification email successfully sent
-                return res.status(200).json({ "status": 200, "message": "Email successfully sent", "info": "Verify Your Email Address" });
+                return res.status(201).json({ "status": 201, "message": "Email successfully sent", "info": "Verify Your Email Address" });
             })
             .catch(err => res.status(500).json({  // failure in Email verification model
                 errors: "Email Failure!",
                 issue: err
             }));
-        
+
     } catch (err) {  // unreocgonized errors
         return res.status(500).json({ status: 500, errors: "Internal server error", issue: err });
     }
@@ -113,12 +116,12 @@ const recoverUserPassword = async (req, res) => {
         const email = req.body.email.toLowerCase();
         let user;
 
-        if (userType === 'user'){  // if user is hitting the forgot password api
+        if (userType === 'user') {  // if user is hitting the forgot password api
 
             // now find that the user exists
             user = await User.findOne({ email });
         }
-        else if (userType === 'admin'){  // if admin is hitting the forgot password api
+        else if (userType === 'admin') {  // if admin is hitting the forgot password api
 
             // now find that the admin exists
             user = await Admin.findOne({ email });
@@ -131,7 +134,7 @@ const recoverUserPassword = async (req, res) => {
         if (!user) return res.status(404).json({ status: 404, message: "User Not Found!" });
 
         // confirm that the user has generated the otp
-        let pin = await RecoverPassword.findOne({ email }); 
+        let pin = await RecoverPassword.findOne({ email });
         if (!pin) return res.status(400).json({ status: 400, message: "You need to Generate OTP" });
 
         // if otp is cleared after password updation
@@ -155,9 +158,9 @@ const recoverUserPassword = async (req, res) => {
 
         // notify the user that password has been changed
         sendMail({
-                to: user.email,
-                subject: "BZML Password generated successfully",
-                html: emailTemp
+            to: user.email,
+            subject: "BZML Password generated successfully",
+            html: emailTemp
         });
 
         // password updated successfully
@@ -170,7 +173,56 @@ const recoverUserPassword = async (req, res) => {
 
 // to generate the otp for mobile verification
 const generateMobileOtp = async (req, res) => {
-    res.send("ok");
+    try {
+        // verify that the user exists
+        let user = await User.findById(req.user.id);
+        if (!user) return res.status(404).json({ status: 404, message: "User Not Found!" });
+
+        // if user's mobile number is verified
+        if (user.isMobileVerified) return res.status(200).json({ status: 200, message: "User is already verified!" });
+
+        // generate the OTP 
+        const otp = generateOtp();
+
+        // now, check that the user is not generated otp yet
+        let mobile = await MobileVerification.findOne({ mobileNumber: user.mobileNumber });
+        if (mobile) {
+
+            // email exists in the Email Verification model
+            return res.status(400).json({ "status": 400, "message": "Mobile verification failed", "info": "try after 15 mins" });
+        }
+
+        // now save the otp in the verificaiton model
+        MobileVerification.create({
+            mobileNumber: user.mobileNumber,
+            otpMobile: otp,
+        })
+            .then((verify) => {  // otp is now saved in the db
+
+                // creating twilio object
+                const client = new Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+                // now, sending message to the client
+                client.messages
+                    .create({
+                        body: `Hello ${user.fullName}, ${verify.otpMobile} is your verification OTP to verify your bzml registerd Mobile Number. Thank you!`,
+                        to: '+91' + user.mobileNumber, // Text your number
+                        from: process.env.TWILIO_MOBILE_NUMBER, // From a valid Twilio number
+                    })
+                    .then(msg => {  // successfully otp sent to the registered mobile number
+                        return res.status(201).json({ "status": 201, "message": "SMS successfully sent!!", "info": "Verify Your Mobile Number" });
+                    })
+                    .catch(err => {  // Twilio service failed
+                        return res.status(500).json({ status: 500, message: "SMS Failure!!" });
+                    });
+            })
+            .catch(err => {
+                return res.status(500).json({ status: 500, message: "Problem with Mobile verifcation model!" });
+            });
+
+    } catch (err) {  // unreocgonized errors
+        return res.status(500).json({ status: 500, errors: "Internal server error", issue: err });
+    }
 };
 
 // export all functions
